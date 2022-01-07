f7e3c2095705441c951edd1cd12219b36c440639
robtuh221 <robtuh221@gmail.com>
2022-01-07 14:03:45 +0300

    finished the project

---
+++ orders/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from .models import Order
+
+
+def order_create(request):
+    cart = Cart(request)
+    form = OrderCreateForm(request.POST)
+    if form.is_valid():
+        order = form.save(commit=False)
+        if cart.coupon:
+            order.coupon = cart.coupon
+        order.discount = cart.coupon.discount
+        order.save()
+        for item in cart:
+            OrderItem.objects.create(order=order, product=item['product'],
+                                     price=item['price'], quantity=item['quantity'])
+        # Очищаем корзину.
+        cart.clear()
+        return render(request, 'orders/order/created.html',
+                      {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html',
+                  {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html', {'order': order})
